74f2e465df9d161bf27a691989c594c8514266cf
Andrej <aacevski@gmail.com>
2025-02-03 00:12:30 +0100

    ci: deploying on hetzner

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,20 +1,40 @@
+import TanStackRouterDevtools from "@/tanstack/router";
 import type { LoggedInUser } from "@/types/user";
 import type { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import {
+  Outlet,
+  createRootRouteWithContext,
+  redirect,
+} from "@tanstack/react-router";
 
 export const Route = createRootRouteWithContext<{
   queryClient: QueryClient;
   user: LoggedInUser | null | undefined;
 }>()({
   component: RootComponent,
+  async beforeLoad({ context: { user }, location }) {
+    const isRouteUnprotected = location.pathname.includes("auth");
+    const isOnDashboard = location.pathname.includes("dashboard");
+
+    if (user === null && !isRouteUnprotected) {
+      throw redirect({
+        to: "/auth/sign-in",
+      });
+    }
+
+    if (user && !isOnDashboard) {
+      throw redirect({
+        to: "/dashboard",
+      });
+    }
+  },
 });
 
 function RootComponent() {
   return (
     <>
-      <div className="flex w-full h-screen flex-col md:flex-row bg-zinc-50 dark:bg-zinc-950 scrollbar-thin scrollbar-thumb-zinc-700 scrollbar-track-zinc-900">
+      <div className="flex w-full h-screen flex-row bg-zinc-50 dark:bg-zinc-950 scrollbar-thin scrollbar-thumb-zinc-700 scrollbar-track-zinc-900">
         <Outlet />
       </div>
       <ReactQueryDevtools buttonPosition="top-right" />
