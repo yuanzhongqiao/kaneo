c3ba0e244a0d86a7390d97b145e0330b795d8410
Andrej <aacevski@gmail.com>
2025-02-16 00:28:29 +0100

    fix: making sidebar on mobile floating

--- apps/web/src/components/common/sidebar/index.tsx
+++ apps/web/src/components/common/sidebar/index.tsx
@@ -5,20 +5,40 @@ import SidebarFooter from "./sidebar-footer";
 import { SidebarHeader } from "./sidebar-header";
 
 export function Sidebar() {
-  const { isSidebarOpened } = useUserPreferencesStore();
+  const { isSidebarOpened, setIsSidebarOpened } = useUserPreferencesStore();
 
   return (
-    <div
-      className={cn(
-        "flex bg-white dark:bg-zinc-900 border-r border-zinc-200 dark:border-zinc-800 h-screen flex-col",
-        "transition-all duration-300 ease-in-out",
-        "min-h-screen",
-        !isSidebarOpened ? "w-16" : "w-64",
-      )}
-    >
-      <SidebarHeader />
-      <SidebarContent />
-      <SidebarFooter />
-    </div>
+    <>
+      <div
+        onClick={() => setIsSidebarOpened()}
+        onKeyDown={(e) => {
+          if (e.key === "Escape") {
+            setIsSidebarOpened();
+          }
+        }}
+        className={cn(
+          "fixed inset-0 bg-black/50 z-40 lg:hidden",
+          isSidebarOpened ? "opacity-100" : "opacity-0 pointer-events-none",
+          "transition-opacity duration-300 ease-in-out",
+        )}
+      />
+
+      <div className="w-16 shrink-0 lg:hidden" />
+
+      <div
+        className={cn(
+          "flex bg-white dark:bg-zinc-900 border-r border-zinc-200 dark:border-zinc-800 h-screen flex-col",
+          "transition-all duration-300 ease-in-out",
+          "min-h-screen",
+          "fixed lg:relative top-0 left-0",
+          "z-50",
+          !isSidebarOpened ? "w-16" : "w-64",
+        )}
+      >
+        <SidebarHeader />
+        <SidebarContent />
+        <SidebarFooter />
+      </div>
+    </>
   );
 }
